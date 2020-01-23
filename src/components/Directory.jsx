c9095d5e7f37e21bb36e3bdca29c343f76ae5531
Giovani Aguirre <me@its-g.io>
2020-01-23 08:00:30 -0600

    Added buttons and styles to manipulate data

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -5,6 +5,11 @@ const Directory = (props) => {
   return (
     <div className="directory">
       <DirectionBtns handleDir={props.handleDir} way="-1" dir="< Previews" />
+      <div className="manipBtns">
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>New</button>
+      </div>
       <DirectionBtns handleDir={props.handleDir} way="1" dir="Next >" />
     </div>
   )
